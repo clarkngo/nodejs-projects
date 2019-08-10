778b457464a43644b8cdde630b7c640a68974f6b
Clark Ngo <clarkngo@gmail.com>
2019-08-10 14:06:51 -0700

    Submission for Lab 1

---
+++ CS224/Clark/Lab1/app.js
@@ -0,0 +1,19 @@
+'use strict'
+const http = require('http');
+
+const port = 4000;
+const hostname = 'localhost';
+
+const server = http.createServer((req, res) => {
+  let visitor_ip = req.connection.remoteAddress;
+  let content;
+  if (req.url === '/about') content = '<h3>This is the ABOUT page.</h3>';
+  else if (req.url === '/address') content = '<h2>This is my address page</h2>';
+  else if (req.url === '/') content = '<h1>This is the HOME page</h1>';
+  else content = '<h2>404 Page is not found.</h2>';
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end(content);
+});
+
+server.listen(port,hostname, () => console.log(`Visitor's IP address is ${visitor_ip}`));

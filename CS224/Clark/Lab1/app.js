bf8d0e70b19fdef1b4c10e73c0b2271270e1bf26
Clark Ngo <clarkngo@gmail.com>
2019-08-10 15:11:30 -0700

    test remote IP

--- CS224/Clark/Lab1/app.js
+++ CS224/Clark/Lab1/app.js
@@ -10,12 +10,12 @@ const server = http.createServer((req, res) => {
   else if (req.url === '/address') content = '<h2>This is my address page</h2>';
   else if (req.url === '/') content = '<h1>This is the HOME page</h1>';
   else content = '<h2>404 Page is not found.</h2>';
-  let visitor_ip = req.connection.remoteAddress;
-  if (visitor_ip) content += '\n' + visitor_ip;
+  let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  if (ip) content += '\n' + ip;
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end(content);
 });
 
-server.listen(port,hostname, () => console.log(`Visitor's IP address is `));
+server.listen(port,hostname, () => console.log(`Server is running at http://${hostname}:${port}`));

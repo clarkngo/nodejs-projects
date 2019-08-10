c67c73e6d40c2bc70f7ab0d73f887f7da31f2586
Clark Ngo <clarkngo@gmail.com>
2019-08-10 14:40:20 -0700

    test remote IP

--- CS224/Clark/Lab1/app.js
+++ CS224/Clark/Lab1/app.js
@@ -10,8 +10,9 @@ const server = http.createServer((req, res) => {
   else if (req.url === '/address') content = '<h2>This is my address page</h2>';
   else if (req.url === '/') content = '<h1>This is the HOME page</h1>';
   else content = '<h2>404 Page is not found.</h2>';
-  let visitor_ip = req.connection.remoteAddress
-  console.log(`${visitor_ip}`)
+  let visitor_ip = req.connection.remoteAddress;
+  if (visitor_ip) content += '\n' + visitor_ip;
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end(content);
